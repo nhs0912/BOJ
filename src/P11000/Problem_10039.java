4f1ee583b772d97461606de3618099de2427b8c0
heeSeok <nhs0912@nate.com>
2016-12-15 00:27:10 +0900

    add a first Problem_10039

---
+++ src/P11000/Problem_10039.java
@@ -0,0 +1,23 @@
+package P11000;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * Created by nhs0912 on 2016-12-15.
+ */
+public class Problem_10039 {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+        int score = 0;
+        for (int i = 0; i < 5; i++) {
+            score = sc.nextInt();
+            if (score < 40) {
+                score = 40;
+            }
+            sum += score;
+        }
+        System.out.println(sum / 5);
+    }
+}
