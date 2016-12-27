f6b0df515a773199bf16edb9646173f1ccb4614d
nhs0912 <nhs0912@nate.com>
2016-12-28 00:53:12 +0900

    Count Sort - 미완성

--- src/Main.java
+++ src/Main.java
@@ -1,42 +1,73 @@
 import java.util.Scanner;
 
 public class Main {
-    Main() {
-        int num = scan();
-        String txt = whatFraction(num);
-        Print(txt);
-    }
+    int[] numbers; //입력된 숫자
+    int[] countArr;//숫자 세기
+    int[] result; //정렬된 후 숫자 저장
 
-    int scan() {
+    void inputNumbers() {//숫자 입력하기
         Scanner sc = new Scanner(System.in);
-        return sc.nextInt();
+        int size = sc.nextInt();
+        numbers = new int[size];
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = sc.nextInt();
+        }
     }
 
-    static String whatFraction(int num) {
-        int up = 0; //분자
-        int down = 0; //분모
-        int n = 1;
-        String txt;
-        String Fraction;
-        //Boolean exitCheck = false;
-        for (int i = 1; i <= num; i++) {
-            for (int j = 1; j <= i; j++) {
-                if (i % 2 != 0) {//홀수 일 때
-                    up = i;
-
-                }
-                txt = up + "/" + down;
+    int findMaxNumber() {//최댓값 찾기
+        int max = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            if (max < numbers[i]) {
+                max = numbers[i];
             }
         }
-
-        return txt;
+        return max;
     }
 
-    void Print(String txt) {
-        //System.out.println(txt);
+    void display() {
+        for (int i = 0; i < countArr.length; i++) {
+            System.out.print(countArr[i] + " ");
+        }
+        System.out.println();
     }
+
+    void display(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    void sort() {
+        inputNumbers();
+        int maxNumber = findMaxNumber();//최댓값 저장
+        countArr = new int[maxNumber + 1]; //0-maxNumber+1만큼 생성
+        result = new int[numbers.length];
+
+        for (int i = 0; i < numbers.length; i++) {
+            //해당하는 숫자 카운터
+            countArr[numbers[i]]++;
+        }
+        System.out.println("CountArr[]=");
+        display();
+
+        for (int i = 1; i < countArr.length; i++) {
+            //누적 숫자 더하기
+            countArr[i] += countArr[i - 1];
+        }
+        System.out.println("누적 배열");
+        display();
+
+        for (int i = numbers.length - 1; i >= 0; i--) {
+            //정렬하기
+            result[countArr[numbers[i]] - 1] = numbers[i];
+            countArr[numbers[i]]--;
+        }
+        display(result);
+    }
+
 
     public static void main(String[] args) {
-        new Main();
+        new Main().sort();
     }
 }
---
+++ src/Test.java
@@ -0,0 +1,144 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class ListNode {
+    int data;
+    ListNode link;
+
+    ListNode() {
+        this.data = 0;
+        this.link = null;
+    }
+
+    ListNode(int num) {
+        this.data = num;
+
+    }
+
+    ListNode(int num, ListNode link) {
+        this.data = num;
+        this.link = link;
+    }
+
+    public int getData() {
+        return this.data;
+    }
+}
+
+class LinkedList {
+
+    private ListNode head;
+
+    public LinkedList() {
+        head = null;
+
+    }
+
+    public void insertMiddleNode(ListNode pre, int num) {
+        ListNode newNode = new ListNode(num);
+        newNode.link = pre.link;
+        pre.link = newNode;
+    }
+
+    public void insertLastNode(int num) {
+        ListNode newNode = new ListNode(num);
+        if (head == null) {
+            this.head = newNode;
+
+        } else {
+            ListNode temp = head;
+            while (temp.link != null) {
+                temp = temp.link;
+            }
+            temp.link = newNode;
+        }
+    }
+
+    public void deleteLastNode() {
+        ListNode pre, temp;
+        if (head == null) {
+            return;
+        }
+        if (head.link == null) {
+            head = null;
+        } else {
+            pre = head;
+            temp = head.link;
+            while (temp.link != null) {
+                pre = temp;
+                temp = temp.link;
+            }
+            pre.link = null;
+
+        }
+    }
+
+    public ListNode searchNode(int num) {
+        ListNode temp = this.head;
+        while (temp != null) {
+            if (num == temp.getData()) {
+                return temp;
+            } else {
+                temp = temp.link;
+            }
+        }
+        return temp;
+    }
+
+
+}
+
+class Stack {
+
+    ListNode top;
+
+    //Boolean isFull(){
+//
+//    return
+//}
+    Boolean isEmpty() {
+        return (top == null);
+    }
+
+    public void push(int num) {
+        ListNode newNode = new ListNode();
+        newNode.data = num;
+        newNode.link = top;
+        top = newNode;
+
+    }
+
+    public int pop() {
+        if (isEmpty()) {
+            return 0;
+        } else {
+            int num = top.data;
+            top = top.link;
+            return num;
+        }
+    }
+
+    public int peek() {
+        if (isEmpty()) {
+            return 0;
+        } else {
+            return top.data;
+        }
+    }
+
+}
+
+public class Test {
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        while (st.hasMoreTokens()) {
+            System.out.println(st.nextToken());
+        }
+    }
+}
+
