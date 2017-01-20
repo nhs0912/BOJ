4967294950c85f3bb2f670b604e1a0688147a686
nhs0912 <nhs0912@nate.com>
2017-01-20 12:24:46 +0900

    문제 미완성

--- src/Problems/P_2581.java
+++ src/Problems/P_2581.java
@@ -1,22 +1,23 @@
-import java.io.*;
+package Problems;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 /**
- * 2017-01-20
- * 문제 번호 : 9020
- * 문제 이름 : 골드바흐의 추측
- * 문제 사이트 주소 : https://www.acmicpc.net/problem/9020
+ * 2017-01-19
+ * 문제 번호 : 2581
+ * 문제 이름 : 소수
+ * 문제 사이트 주소 : https://www.acmicpc.net/problem/2581
  */
-class Main {
+class P_2581 {
 
     int[] numbers;
     ArrayList<Integer> primeNumbers = new ArrayList<>();
-    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    int cnt = 0;//PrimeNumber count
-    int[] smallPrimeNumber;
+    int sum = 0;
 
     boolean isPrimeNumber(int n) {
-
         if (numbers[n] != 1) {
             //제거 되지 않은 수라면
             for (int i = 0; i < primeNumbers.size(); i++) {
@@ -43,41 +44,43 @@ class Main {
     }
 
     void display(int n, int m) throws IOException {
-        cnt = 0;
-        System.out.println("소수 갯수 = " + primeNumbers.size());
-        System.out.println("primeNumbers ");
-        for(int i=0; i<primeNumbers.size();i++)
-        {
-            System.out.print(primeNumbers.get(i)+" ");
-        }
-        System.out.println();
+        //BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+       // StringBuffer sb = new StringBuffer();
+        int firstPrimeNumber = 0;
+        boolean isFirst = true;
+        boolean existPrimeNumber = false;
         for (int i = n; i <= m; i++) {
             if (numbers[i] == 0 && i != 1 && i != 0) {
-                cnt++;
-                System.out.print(i + " ");
+                if (isFirst) {
+                    firstPrimeNumber = i;
+                    isFirst = false;
+                    existPrimeNumber=true;
+                }
+                sum += i;
             }
+        }
+        if(existPrimeNumber) {
+            System.out.println(sum);
+            System.out.println(firstPrimeNumber);
+        }else{
+            System.out.println(-1);
         }
 
     }
 
     void Solve() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine().trim());
-        while (T-- > 0) {
-            int n = 2;
-            int m = Integer.parseInt(br.readLine().trim());
-            numbers = new int[m + 1];
-            getPrimeNumber(m);
-            display(n, m);
-        }
 
-        bw.close();
+        int n = Integer.parseInt(br.readLine().trim());
+        int m = Integer.parseInt(br.readLine().trim());
+        numbers = new int[m + 1];
+        getPrimeNumber(m);
+        display(n, m);
 
     }
 
     public static void main(String[] args) throws IOException {
-        new Main().Solve();
-
+        new P_2581().Solve();
     }
 
 
