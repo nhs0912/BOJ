90ea04edb86df3be674bbef6bcb5bddd1514de09
HeeSeok <nhs0912@nate.com>
2017-01-29 13:28:33 +0900

    퀵소트

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,57 @@
-/**
- * 문제 번호 :
- * 문제 이름 :
- * 문제 사이트 :
- */
-class Main{
-    public static void main(String[] args) {
+class Sort{
+    int i=0;
+    public int partition(int a[], int begin, int end){
+        int pivot, temp, L, R, t;
 
+        L = begin;
+        R = end;
+        pivot = (begin + end)/2;
+        System.out.printf("\n [Quick Sort %d : pivot=%d ]\n", ++i, a[pivot]);
+        while(L<R){
+            while((a[L]<a[pivot]) && (L<R)) L++;
+            while((a[R]>=a[pivot]) && (L<R)) R--;
+            if(L<R){
+                temp = a[L];
+                a[L] = a[R];
+                a[R] = temp;
+
+                //////
+                if(L==pivot)
+                    pivot = R;
+                /////
+            }
+        }
+        //(L=R)??????
+
+        temp = a[pivot];
+        a[pivot] = a[R];
+        a[R] = temp;
+        for(t=0; t<a.length; t++)
+            System.out.printf("%3d  ", a[t]);
+        System.out.println();
+        return R;
+    }
+
+    public void quickSort(int a[], int begin, int end){
+        if(begin < end){
+            int p;
+            p = partition(a, begin, end);
+            quickSort(a, begin, p-1);
+            quickSort(a, p+1, end);
+        }
+    }
+}
+
+
+public class Main {
+    public static void main(String args[]){
+        int a[]={5, 1, 4, 2, 3};
+        int size=a.length - 1;
+        Sort S = new Sort();
+        System.out.printf("\nQuick Sort : ");
+        for(int i=0; i<=size; i++)
+            System.out.printf(" %d", a[i]);
+        System.out.println();
+        S.quickSort(a, 0, size);
     }
 }
